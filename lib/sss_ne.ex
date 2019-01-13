e028ad4eb1035f73197e1dc22802e16011e04b08
MikaAK <mika@kalathil.me>
2019-01-12 22:38:13 -0800

    chore: Initialize Repo

---
+++ lib/sss_ne.ex
@@ -0,0 +1,18 @@
+defmodule SssNe do
+  @moduledoc """
+  Documentation for SssNe.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> SssNe.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/sss_ne_test.exs
@@ -0,0 +1,8 @@
+defmodule SssNeTest do
+  use ExUnit.Case
+  doctest SssNe
+
+  test "greets the world" do
+    assert SssNe.hello() == :world
+  end
+end
